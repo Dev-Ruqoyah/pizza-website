8f5b924accde7ac3531e107878e7bad21f654cd1
bossLady0001 <hamzatrukayatabiodun02@gmail.com>
2024-03-14 17:15:22 -0700

    new project

--- dashboard.php
+++ dashboard.php
@@ -1,46 +1,180 @@
-
 <div class="dashboard-content ">
     <div class="row  container-lg-fluid container-md-fluid mx-auto">
-        <div class="col-lg-1 col-md-1 rounded-2 ms-1">
-        <?php
+        <div class="col-lg-1 col-md-1  rounded-2 ms-1">
+       
+            <?php
             include "sidebar.php";
             ?>
+          
         </div>
-        <div class="col-lg-8 col-md-8 mt-3 bg-light mx-auto">
+        <div class="col-lg-8 col-md-8 mt-3 bg-light mx-auto ">
             <div class="dashboard-header d-flex justify-content-between">
                 <h4>Today's menu 😋</h4>
                 <div class="search rounded-pill border d-flex justify-content-around align-items-center px-2">
-                    <input type="text" placeholder= "Search by food name" >
+                    <input type="text" placeholder="Search by food name">
                     <i class="fa fa-search"></i>
                 </div>
             </div>
             <div class="dashboard-image mt-3 rounded-3">
                 <div class="dashboard-image-content ">
-                   <div class="container-image d-flex align-items-center ms-4 pt-5 flex-column ">
-                   <h3 class="text-light">Hello, Rukayat</h3>
-                    <p class="fs-5 text-light">Get free delivery every $20 purchase</p>
-                    <button class="btn  dash-btn">Learn More</button>
-                   </div>
+                    <div class="container-image d-flex align-items-center ms-4 pt-5 flex-column ">
+                        <h3 class="text-light">Hello, Rukayat</h3>
+                        <p class="fs-5 text-light">Get free delivery every $20 purchase</p>
+                        <button class="btn  dash-btn">Learn More</button>
+                    </div>
                 </div>
             </div>
-            <div class="dashboard-category">
-                <div class="d-flex flex-no-wrap mt-3">
-                    <div  class="col-lg-2 col-md-2 col-4 d-flex align-items-center dash-product flex-column py-3 rounded-3">
+            <div class="dashboard-category mt-5">
+                <div class="row  flex-nowrap gap-2 mx-auto  mt-3 ovx">
+                    <div class="col-lg-2 col-md-2 col-4 mx-auto d-flex align-items-center dash-product flex-column py-3 rounded-3">
                         <div class="image-pizza rounded-circle">
                             <img src="./images/dashboard.png" alt="" class="rounded-circle">
                         </div>
                         <div class="product-name">
-                           <h5>Pizza</h5>
+                            <h5>Pizza</h5>
                         </div>
                         <div class="product-icon p-3 rounded-circle text-light">
-                        <i class="fa-solid fa-arrow-right"></i>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-4 mx-auto d-flex align-items-center dash-product flex-column py-3 rounded-3">
+                        <div class="image-pizza rounded-circle">
+                            <img src="./images/burgeree.png" alt="" class="rounded-circle">
+                        </div>
+                        <div class="product-name">
+                            <h5>Burger</h5>
+                        </div>
+                        <div class="product-icon p-3 rounded-circle text-light">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-4 mx-auto d-flex align-items-center dash-product flex-column py-3 rounded-3">
+                        <div class="image-pizza rounded-circle">
+                            <img src="./images/burgeree.png" alt="" class="rounded-circle">
+                        </div>
+                        <div class="product-name">
+                            <h5>Burger</h5>
+                        </div>
+                        <div class="product-icon p-3 rounded-circle text-light">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-4 mx-auto d-flex align-items-center dash-product flex-column py-3 rounded-3">
+                        <div class="image-pizza rounded-circle">
+                            <img src="./images/burgeree.png" alt="" class="rounded-circle">
+                        </div>
+                        <div class="product-name">
+                            <h5>Burger</h5>
+                        </div>
+                        <div class="product-icon p-3 rounded-circle text-light">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-4 mx-auto d-flex align-items-center dash-product flex-column py-3 rounded-3">
+                        <div class="image-pizza rounded-circle">
+                            <img src="./images/burgeree.png" alt="" class="rounded-circle">
+                        </div>
+                        <div class="product-name">
+                            <h5>Burger</h5>
+                        </div>
+                        <div class="product-icon p-3 rounded-circle text-light">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="dash-product-container">
+                <div class="row flex-nowrap mt-3 ovx">
+                    <div class="col-lg-4 col-md-6 col-12 ">
+                        <div class="dashproduct-image mx-auto">
+                            <img src="./images/dashboard.png" alt="">
+                        </div>
+                        <div class="dashproduct-details p-2 rounded-3 pt-5 pb-3 d-flex align-items-center justify-content-between">
+                            <div class="dash-one pt-3">
+                                <h5>Mushroom Pizza</h5>
+                                <div class="star d-flex">
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                </div>
+                                <div class="dash-price">
+                                    <p>$342</p>
+                                </div>
+                            </div>
+                            <div class="dash-two p-3 rounded-circle pt-3">
+                                <i class="fa fa-plus text-light"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12 ">
+                        <div class="dashproduct-image mx-auto">
+                            <img src="./images/dashboard.png" alt="">
+                        </div>
+                        <div class="dashproduct-details p-2 rounded-3 pt-5 pb-3 d-flex align-items-center justify-content-between">
+                            <div class="dash-one pt-3">
+                                <h5>Mushroom Pizza</h5>
+                                <div class="star d-flex">
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                </div>
+                                <div class="dash-price">
+                                    <p>$342</p>
+                                </div>
+                            </div>
+                            <div class="dash-two p-3 rounded-circle pt-3">
+                                <i class="fa fa-plus text-light"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12 ">
+                        <div class="dashproduct-image mx-auto">
+                            <img src="./images/dashboard.png" alt="">
+                        </div>
+                        <div class="dashproduct-details p-2 rounded-3 pt-5 pb-3 d-flex align-items-center justify-content-between">
+                            <div class="dash-one pt-3">
+                                <h5>Mushroom Pizza</h5>
+                                <div class="star d-flex">
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                    <i class="fa fa-star text-warning   "></i>
+                                </div>
+                                <div class="dash-price">
+                                    <p>$342</p>
+                                </div>
+                            </div>
+                            <div class="dash-two p-3 rounded-circle pt-3">
+                                <i class="fa fa-plus text-light"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 bg-light mx-auto mt-3 rounded-3">
-
+       
+        <div class="col-lg-3 col-md-3 bg-main mx-auto mt-4 rounded-3">
+            
+            <div class="dash-notification p-3 d-flex justify-content-around align-item-center">
+                <div class="icon-dash mt-2">
+                    <i class="fa fa-bell text-light"></i>
+                </div>
+                <div class="dash-profile d-flex">
+                    <div class="dashprofile-image">
+                        <img src="./images/dashboard.png" alt="">
+                    </div>
+                    <div class="dashuser-name text-light">
+                        <h6 class="m-0">Rukayat</h6>
+                        <p>User</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -48,4 +182,5 @@
 
 
 </body>
+
 </html>
--- sidebar.php
+++ sidebar.php
@@ -8,7 +8,7 @@
 
 
     <!-- CUSTOM CSS -->
-    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="style.css" />
 
     <!--FONTAWESOME LINK -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
@@ -24,6 +24,11 @@
     <div class="sidebar-container">
         <div style= "background-color: var(--primary-color); color: white;"  class="  text-white py-3 sidebar mt-lg-3 mt-md-2 mt-0 rounded-3">
             <div class="container-up ">
+            <div class="lines d-flex flex-column gap-1 m-auto">
+                <div class="line bg-light"></div>
+                <div class="line bg-light"></div>
+                <div class="line bg-light"></div>
+            </div>
                 <div class="home p-3 rounded-circle ">
                    <a class="text-decoration-none " href="#"> <i class="fa-solid fa-home"></i></a>
                 </div>
